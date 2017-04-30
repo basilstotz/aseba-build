09914dc2393a51192e20765e25f4e4cf55b4793b
basilstotz <basil.stotz@gmail.com>
2017-04-30 12:02:29 +0200

    Update aseba-build.sh

--- aseba-build.sh
+++ aseba-build.sh
@@ -1,10 +1,7 @@
-#apt-get install libprotobuf-dev protobuf-compiler zzuf valgrind libboost-python-dev libpython-dev libboost-dev libqt4-dev qt4-dev-tools libqwt5-qt4-dev libudev-dev libsdl2-dev libxml2-dev libqtwebkit-dev cmake g++ git make
+#!/bin/sh
 
-
-# create build tree
-#mkdir -p aseba/build-dashel aseba/build-enki aseba/build-aseba
 cd aseba
-# fetch and compile dashel
+# pull and compile dashel
 cd dashel
 git pull origin
 cd ..
@@ -12,7 +9,7 @@ cd build-dashel
 cmake ../dashel -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_SHARED_LIBS=ON
 make
 cd ..
-# fetch and compile enki
+# pull and compile enki
 cd enki
 git pull origin
 cd ..
@@ -20,7 +17,7 @@ cd build-enki
 cmake ../enki -DCMAKE_BUILD_TYPE=RelWithDebInfo
 make
 cd ..
-# fetch and compile aseba, telling it where to find dashel and enki
+# pull and compile aseba, telling it where to find dashel and enki
 cd aseba
 git pull origin && git checkout release-1.5.x && git submodule update --init && cd ..
 cd build-aseba
