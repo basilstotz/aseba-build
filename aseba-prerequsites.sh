3aedb8e7b6e44fe6aa2c72e68eec78f29c001f0a
basilstotz <basil.stotz@gmail.com>
2017-04-30 11:50:26 +0200

    added doxigen

--- aseba-prerequsites.sh
+++ aseba-prerequsites.sh
@@ -1,3 +1,3 @@
-sudo apt-get install libprotobuf-dev protobuf-compiler zzuf valgrind libboost-python-dev libpython-dev libboost-dev libqt4-dev qt4-dev-tools libqwt5-qt4-dev libudev-dev libsdl2-dev libxml2-dev libqtwebkit-dev cmake g++ git make
+sudo apt-get install doxigen libprotobuf-dev protobuf-compiler zzuf valgrind libboost-python-dev libpython-dev libboost-dev libqt4-dev qt4-dev-tools libqwt5-qt4-dev libudev-dev libsdl2-dev libxml2-dev libqtwebkit-dev cmake g++ git make
 
 
