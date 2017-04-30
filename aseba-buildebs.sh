7192d0f882e6f2027a9b3eb064439f59772f038e
basilstotz <basil.stotz@gmail.com>
2017-04-30 11:49:17 +0200

    Update aseba-buildebs.sh

--- aseba-buildebs.sh
+++ aseba-buildebs.sh
@@ -1,9 +1,22 @@
-cd dashel
-dpkg-buildpackage -uc -tc
-cd ..
-cd enki
-dpkg-buildpackage -uc -tc
-cd ..
+
+# dpkg-buildpackage -uc -tc
 cd aseba
-dpkg-buildpackage -uc -tc
+# build Dashel package and install it
+cd dashel
+sudo mk-build-deps -i         # install dependencies
+debuild -i -us -uc -b         # build package
 cd ..
+sudo dpkg -i libdashel*.deb   # install package
+# build Enki package and install it
+cd enki
+sudo mk-build-deps -i         # install dependencies
+debuild -i -us -uc -b         # build package
+cd ..
+sudo dpkg -i libenki*.deb     # install package
+# build Aseba package
+cd aseba
+sudo mk-build-deps -i         # install dependencies
+debuild -i -us -uc -b         # build package
+cd ..
+# remove libdashel and libenki
+sudo apt-get -y remove libdashel libenki
