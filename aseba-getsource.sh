01d2951c5719577ff407a16f4789a83fd9f3567b
basilstotz <basil.stotz@gmail.com>
2017-04-30 12:16:20 +0200

    Update aseba-getsource.sh

--- aseba-getsource.sh
+++ aseba-getsource.sh
@@ -1,16 +1,27 @@
 #!/bin/sh
 
-if test -d aseba/aseba; then
-  echo "alreay fetched"
-  exit 0
-fi
-
-# create build tree
-mkdir -p aseba/build-dashel aseba/build-enki aseba/build-aseba
 cd aseba
-# fetch dashel
-git clone git://github.com/aseba-community/dashel.git
-# fetch enki
-git clone git://github.com/enki-community/enki.git
-# fetch aseba
-git clone git://github.com/aseba-community/aseba.git
+
+if test -d aseba; then
+  echo "git pull origin"
+  # pull and compile dashel
+  cd dashel
+  git pull origin
+  cd ..
+  # pull and compile enki
+  cd enki
+  git pull origin
+  cd ..
+  # pull aseba
+  #git pull origin && git checkout release-1.5.x && git submodule update --init && cd ..
+  git pull origin && git submodule update --init && cd ..
+else
+  # create build tree
+  mkdir -p aseba/build-dashel aseba/build-enki aseba/build-aseba
+  cd aseba
+  # fetch dashel
+  git clone git://github.com/aseba-community/dashel.git
+  # fetch enki
+  git clone git://github.com/enki-community/enki.git
+  # fetch aseba
+  git clone git://github.com/aseba-community/aseba.git
