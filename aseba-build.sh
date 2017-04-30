b699874a96f06f56f9903f80c6e74f7cd49fd4b4
basilstotz <basil.stotz@gmail.com>
2017-04-30 12:08:36 +0200

    Update aseba-build.sh

--- aseba-build.sh
+++ aseba-build.sh
@@ -19,7 +19,8 @@ make
 cd ..
 # pull and compile aseba, telling it where to find dashel and enki
 cd aseba
-git pull origin && git checkout release-1.5.x && git submodule update --init && cd ..
+#git pull origin && git checkout release-1.5.x && git submodule update --init && cd ..
+git pull origin && git submodule update --init && cd ..
 cd build-aseba
 export dashel_DIR=../build-dashel
 cmake ../aseba -DCMAKE_BUILD_TYPE=RelWithDebInfo -Ddashel_DIR=../build-dashel -DDASHEL_INCLUDE_DIR=../dashel -DDASHEL_LIBRARY=../build-dashel/libdashel.so -DENKI_INCLUDE_DIR=../enki -DENKI_LIBRARY=../build-enki/enki/libenki.a -DENKI_VIEWER_LIBRARY=../build-enki/viewer/libenkiviewer.a
